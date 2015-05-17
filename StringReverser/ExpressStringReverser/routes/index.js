031da124c5fc51e2711889294032ed77500cc53a
aribornstein <abornst@microsoft.com>
2015-05-16 22:54:30 -0400

    The wrong directory was uploaded before

---
+++ StringReverser/ExpressStringReverser/routes/index.js
@@ -0,0 +1,18 @@
+﻿
+/*
+ * GET home page.
+ */
+
+
+
+exports.index = function (req, res)
+{
+
+    
+    var vm = {
+        title: "String Reverser", 
+        foo: "bar"
+    };
+res.render('index', vm);
+};
+
---
+++ StringReverser/ExpressStringReverser/routes/translate.js
@@ -0,0 +1,39 @@
+﻿/*
+ * GET home page.
+ */
+
+exports.translate = function (req, res) {
+    res.send('this is a sample!');	
+};
+
+
+
+/*
+var client_secret = "XwKRHkdYdxsRY212VvZQhrzmUmbfs0DAqmJEt/6RN08=";
+var client_id = "PythicStringReverser";
+
+console.log(client_id);
+if (!client_secret || !client_id) {
+    console.log('client_secret and client_id missing');
+    process.exit(1);
+}
+
+var params = {
+    text: 'How\'s it going?',
+    from: 'en',
+    to: 'es'
+};
+
+var client = new MsTranslator({
+    client_id: client_id,
+    client_secret: client_secret
+});
+
+client.initialize_token(function () {
+    client.translate(params, function (err, data) {
+        if (err) console.log('error:' + err);
+        console.log(data);
+        process.exit();
+    });
+});
+*/
